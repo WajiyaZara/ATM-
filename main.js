dc9e5a279e5ba3291beea868946ad3b6334d1ebd
WajiyaZara <163625855+WajiyaZara@users.noreply.github.com>
2024-05-17 10:15:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+let myBaklance = 10000; // Dollar
+let mypin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "q1",
+        message: "enteryour pin",
+        type: "number"
+    }
+]);
+// 12345    === 1234 - fales
+if (pinAnswer.pin === mypin) {
+    console.log("Corret pin code!!!");
+}
